4542ec4e9f420d21344be3fd8057b4ba854c63a9
Edward Cochran <ecochra@0587388875.wireless.umich.net>
2018-01-26 13:32:43 -0500

    Committing all initial app files

---
+++ index.js
@@ -0,0 +1,124 @@
+(function() {
+    onload = () => {
+        var http = new XMLHttpRequest(),
+            url = "http://localhost:3000/data"
+
+        http.onreadystatechange = () => {
+            if (http.readyState === 4 && http.status === 200) {
+                var data = JSON.parse(http.responseText)
+
+                console.log(data)
+
+                data.forEach(append_data)
+
+            }
+        }
+
+        http.open("GET", url)
+        http.send()
+    }
+
+    var btn = document.getElementById("post"),
+        table = document.querySelector("table");
+
+    btn.addEventListener("click", () => {
+        var date = document.getElementById("date"),
+            hours = document.getElementById("hours"),
+            task = document.getElementById("task"),
+            obj = {
+                date: date.value,
+                hours: hours.value,
+                task: task.value
+            }
+
+        post_data(obj)
+
+        date.value = ""
+        hours.value = ""
+        task.value = ""
+
+    })
+
+    table.addEventListener("click", (e) => {
+        if (e.target.className === "delete") {
+            console.log(e.target.parentNode.parentNode.id)
+            delete_data(e.target.parentNode.parentNode.id)
+        }
+    })
+
+
+    function post_data(data) {
+        var http = new XMLHttpRequest(),
+            url = "http://localhost:3000/data",
+            data = JSON.stringify(data);
+
+        http.onreadystatechange = () => {
+            if (http.readyState === 4 && http.status === 201) {
+                var item = JSON.parse(http.responseText)
+                console.log(item)
+                append_data(item)
+            }
+        }
+
+        http.open("POST", url)
+        http.setRequestHeader("Content-Type", "application/json", true)
+        http.send(data)
+
+    }
+
+    function get_data() {
+        var http = new XMLHttpRequest(),
+            url = "http://localhost:3000/data"
+
+        http.onreadystatechange = () => {
+            if (http.readyState === 4 && http.status === 200) {
+                var data = JSON.parse(http.responseText)
+
+                console.log(data)
+
+                append_data(data)
+            }
+        }
+
+        http.open("GET", url)
+        http.send()
+    }
+
+    function append_data(data) {
+        var html = `
+            <td>${data.date}</td>
+            <td>${data.hours}</td>
+            <td>${data.task} <button class="delete">Delete</button></td>
+        `,
+            tr = document.createElement("tr"),
+            tbody = document.querySelector("tbody");
+
+        tr.innerHTML = html;
+        tr.id = `${data.id}`
+        tr.className = "task-data"
+
+        tbody.appendChild(tr)
+
+
+    }
+
+    function delete_data(data) {
+        var http = new XMLHttpRequest(),
+            url = `http://localhost:3000/data/${data}`,
+            el = document.getElementById(`${data}`);
+
+        console.log(url)
+
+        http.open("DELETE", url , true)
+        http.onreadystatechange = () => {
+            if (http.readyState === 4 && http.status === 200) {
+                console.log(http.responseText)
+            }
+        }
+        http.send(null)
+
+        el.parentNode.removeChild(el)
+    }
+
+
+}())
